8d6198619b7f12e24797fd1d9f22fba1c02677ec
Misha Pachin <misha.pachin@gmail.com>
2023-01-29 17:58:10 +0100

    Initial commit

---
+++ lib/alias_formatter.ex
@@ -0,0 +1,18 @@
+defmodule AliasFormatter do
+  @moduledoc """
+  Documentation for `AliasFormatter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AliasFormatter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alias_formatter_test.exs
@@ -0,0 +1,8 @@
+defmodule AliasFormatterTest do
+  use ExUnit.Case
+  doctest AliasFormatter
+
+  test "greets the world" do
+    assert AliasFormatter.hello() == :world
+  end
+end
