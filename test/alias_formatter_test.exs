2c4370af74dae4028360a1c28d96049356e32c7c
Misha Pachin <misha.pachin@gmail.com>
2023-03-26 10:45:29 +0200

    Add test for checking aliases hoisting inside module definitions

--- test/alias_formatter_test.exs
+++ test/alias_formatter_test.exs
@@ -43,4 +43,33 @@ defmodule AliasFormatterTest do
       assert String.trim(expected_result) == AliasFormatter.format(test_input, [])
     end
   end
+
+  test "should hoist aliases to the beginning of the module" do
+    test_input = """
+    defmodule TestModuleExample do
+      def first do
+        "first"
+      end
+      alias C_TestModuleExample.Aaa.Ccc
+      def second, do: "second"
+      alias B_TestModuleExample.Bbb.Bbb
+      def third, do: "third"
+      alias A_TestModuleExample.Ccc.Aaa
+    end
+    """
+
+    expected_result = """
+    defmodule TestModuleExample do
+      alias A_TestModuleExample.Ccc.Aaa
+      alias B_TestModuleExample.Bbb.Bbb
+      alias C_TestModuleExample.Aaa.Ccc
+
+      def first, do: "first"
+      def second, do: "second"
+      def third, do: "third"
+    end
+    """
+
+    assert String.trim(expected_result) == AliasFormatter.format(test_input, [])
+  end
 end
