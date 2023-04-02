be4a8b1faee7f9181348812332b1669090cf3180
Misha Pachin <misha.pachin@gmail.com>
2023-04-02 06:51:17 +0200

    Add test case for NamePathsMap.add_alias/2

--- test/alias_formatter/context_alias_collector/name_paths_map_test.exs
+++ test/alias_formatter/context_alias_collector/name_paths_map_test.exs
@@ -3,15 +3,39 @@ defmodule AliasFormatter.ContextAliasCollector.NamePathsMapTest do
 
   alias AliasFormatter.ContextAliasCollector.NamePathsMap
 
-  test "should update given map with alias data" do
-    test_alias_data = {[:Test, :Module, :Aaa], :Aaa}
+  describe "add_alias/2" do
+    test "should update given map with alias data" do
+      test_alias_data = {[:Test, :Module, :Aaa], :Aaa}
 
-    assert %{
-             Test: %{
-               Module: %{
-                 Aaa: :Aaa
+      assert %{
+               Test: %{
+                 Module: %{
+                   Aaa: :Aaa
+                 }
                }
-             }
-           } = NamePathsMap.add_alias(%{}, test_alias_data)
+             } = NamePathsMap.add_alias(%{}, test_alias_data)
+    end
+
+    test "should preserve given map existing paths" do
+      assert %{
+               Test: %{
+                 Module: %{
+                   Aaa: :Aaa
+                 },
+                 Module_2: %{
+                   Aaa: :Aaa_2
+                 }
+               },
+               Test_2: %{
+                 Module: %{
+                   Aaa: :Aaa_3
+                 }
+               }
+             } =
+               %{}
+               |> NamePathsMap.add_alias({[:Test, :Module, :Aaa], :Aaa})
+               |> NamePathsMap.add_alias({[:Test, :Module_2, :Aaa], :Aaa_2})
+               |> NamePathsMap.add_alias({[:Test_2, :Module, :Aaa], :Aaa_3})
+    end
   end
 end
