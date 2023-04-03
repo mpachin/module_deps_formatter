65436c13e02b5fa8b75e01d71d1495e86dcb18f1
Misha Pachin <misha.pachin@gmail.com>
2023-04-03 06:04:57 +0200

    Fix NamePathsMap.add_alias/2; update correspondent tests

--- lib/alias_formatter/context_alias_collector/name_paths_map.ex
+++ lib/alias_formatter/context_alias_collector/name_paths_map.ex
@@ -1,16 +1,22 @@
 defmodule AliasFormatter.ContextAliasCollector.NamePathsMap do
-  def add_alias(%{} = name_paths_map, {name_path, alias_as}) do
-    populate_state_with_alias(name_path, name_paths_map, alias_as)
+  def add_alias({%{} = name_paths_map, %{} = used_names}, {name_path, alias_as})
+      when is_list(name_path) do
+    populate_state_with_alias(name_path, name_paths_map, alias_as, used_names)
   end
 
-  defp populate_state_with_alias([last_module_name], name_paths_map, alias_as) do
-    Map.put(name_paths_map, last_module_name, alias_as)
+  defp populate_state_with_alias([last_module_name], name_paths_map, alias_as, used_names) do
+    {updated_name, updated_used_names} = update_names(alias_as, used_names)
+
+    name_paths_map
+    |> Map.put(last_module_name, updated_name)
+    |> then(&{&1, updated_used_names})
   end
 
   defp populate_state_with_alias(
          [atom_module_name | rest_atom_name_list],
          name_paths_map,
-         alias_as
+         alias_as,
+         used_names
        ) do
     nested_paths_map =
       name_paths_map
@@ -20,9 +26,63 @@ defmodule AliasFormatter.ContextAliasCollector.NamePathsMap do
         nested_state_map -> nested_state_map
       end
 
-    updated_nested_paths_map =
-      populate_state_with_alias(rest_atom_name_list, nested_paths_map, alias_as)
+    {updated_nested_paths_map, updated_used_names} =
+      populate_state_with_alias(rest_atom_name_list, nested_paths_map, alias_as, used_names)
 
-    Map.put(name_paths_map, atom_module_name, updated_nested_paths_map)
+    name_paths_map
+    |> Map.put(atom_module_name, updated_nested_paths_map)
+    |> then(&{&1, updated_used_names})
+  end
+
+  defp update_names(atom_name, used_names) do
+    if Map.has_key?(used_names, atom_name) do
+      {updated_name, updated_increment} =
+        used_names
+        |> Map.get(atom_name)
+        |> increment_postfix(atom_name)
+
+      {updated_name, Map.put(used_names, atom_name, updated_increment)}
+    else
+      {atom_name, Map.put(used_names, atom_name, 1)}
+    end
+  end
+
+  defp increment_postfix(increment, atom_name) do
+    atom_name
+    |> Atom.to_string()
+    |> String.split("_")
+    |> case do
+      [string_name] ->
+        updated_increment = increment + 1
+        {"#{string_name}_#{updated_increment}", updated_increment}
+
+      splitted_name_list ->
+        {postfix, rest_name_list} = List.pop_at(splitted_name_list, -1)
+
+        postfix
+        |> increment_str_postfix()
+        |> case do
+          {:ok, incremented_postfix} ->
+            {rest_name_list ++ [incremented_postfix], incremented_postfix}
+
+          {:error, new_postfix} ->
+            {splitted_name_list ++ [new_postfix], new_postfix}
+        end
+        |> then(fn {name_list, increment} ->
+          {Enum.join(name_list, "_"), increment}
+        end)
+    end
+    |> then(fn {string_name, increment} ->
+      {String.to_atom(string_name), increment}
+    end)
+  end
+
+  defp increment_str_postfix(postfix) do
+    try do
+      int_postfix = String.to_integer(postfix)
+      {:ok, int_postfix + 1}
+    rescue
+      _ -> {:error, 2}
+    end
   end
 end
--- test/alias_formatter/context_alias_collector/name_paths_map_test.exs
+++ test/alias_formatter/context_alias_collector/name_paths_map_test.exs
@@ -7,54 +7,59 @@ defmodule AliasFormatter.ContextAliasCollector.NamePathsMapTest do
     test "should update given map with alias data" do
       test_alias_data = {[:Test, :Module, :Aaa], :Aaa}
 
-      assert %{
-               Test: %{
-                 Module: %{
-                   Aaa: :Aaa
+      assert {
+               %{
+                 Test: %{
+                   Module: %{
+                     Aaa: :Aaa
+                   }
                  }
-               }
-             } = NamePathsMap.add_alias(%{}, test_alias_data)
+               },
+               %{Aaa: 1}
+             } = NamePathsMap.add_alias({%{}, %{}}, test_alias_data)
     end
 
     test "should preserve given map existing paths" do
-      assert %{
-               Test: %{
-                 Module: %{
-                   Aaa: :Aaa
-                 },
-                 Module_2: %{
-                   Aaa: :Aaa_2
-                 }
-               },
-               Test_2: %{
-                 Module: %{
-                   Aaa: :Aaa_3
-                 }
-               }
-             } =
-               %{}
+      assert {%{
+                Test: %{
+                  Module: %{
+                    Aaa: :Aaa
+                  },
+                  Module_2: %{
+                    Aaa: :Aaa_2
+                  }
+                },
+                Test_2: %{
+                  Module: %{
+                    Aaa: :Aaa_3
+                  }
+                }
+              },
+              %{Aaa: 1, Aaa_2: 1, Aaa_3: 1}} =
+               {%{}, %{}}
                |> NamePathsMap.add_alias({[:Test, :Module, :Aaa], :Aaa})
                |> NamePathsMap.add_alias({[:Test, :Module_2, :Aaa], :Aaa_2})
                |> NamePathsMap.add_alias({[:Test_2, :Module, :Aaa], :Aaa_3})
     end
 
     test "should automatically postfix as: alias with increment if leaves collision found" do
-      assert %{
-               Test: %{
-                 Module: %{
-                   Aaa: :Aaa
-                 },
-                 Module_2: %{
-                   Aaa: :Aaa_2
-                 }
-               },
-               Test_2: %{
-                 Module: %{
-                   Aaa: :Aaa_3
-                 }
-               }
-             } =
-               %{}
+      assert {%{
+                Test: %{
+                  Module: %{
+                    Aaa: :Aaa
+                  },
+                  Module_2: %{
+                    Aaa: :Aaa_2
+                  }
+                },
+                Test_2: %{
+                  Module: %{
+                    Aaa: :Aaa_3
+                  }
+                }
+              },
+              %{Aaa: 3}} =
+               {%{}, %{}}
                |> NamePathsMap.add_alias({[:Test, :Module, :Aaa], :Aaa})
                |> NamePathsMap.add_alias({[:Test, :Module_2, :Aaa], :Aaa})
                |> NamePathsMap.add_alias({[:Test_2, :Module, :Aaa], :Aaa})
