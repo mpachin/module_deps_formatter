7d555508052ff214badc221c84a99d25aed51b50
Misha Pachin <misha.pachin@gmail.com>
2023-04-10 13:01:19 +0200

    Add test for ContextAliasCollector.get_result_aliases/1; add implementation

--- lib/alias_formatter/context_alias_collector.ex
+++ lib/alias_formatter/context_alias_collector.ex
@@ -4,12 +4,16 @@ defmodule AliasFormatter.ContextAliasCollector do
   alias AliasFormatter.ContextAliasCollector.NamePathsMap
 
   def add_alias(pid, {name_path, alias_as} = alias_data)
-      when is_list(name_path) and is_atom(alias_as) do
+      when is_pid(pid) and is_list(name_path) and is_atom(alias_as) do
     GenServer.cast(pid, {:add_alias, alias_data})
   end
 
-  def get_short_name(pid, name_path) when is_list(name_path) do
+  def get_short_name(pid, name_path) when is_pid(pid) and is_list(name_path) do
     GenServer.call(pid, {:get_short_name, name_path})
+  end
+
+  def get_result_aliases(pid) when is_pid(pid) do
+    GenServer.call(pid, :get_result_aliases)
   end
 
   def start_link(_) do
@@ -39,5 +43,14 @@ defmodule AliasFormatter.ContextAliasCollector do
     |> then(fn {short_name, updated_state} ->
       {:reply, short_name, updated_state}
     end)
+  end
+
+  @impl true
+  def handle_call(:get_result_aliases, _from, {paths_to_leaf, _, _} = state) do
+    paths_to_leaf
+    |> Enum.sort(fn {name_path_1, _}, {name_path_2, _} ->
+      Enum.join(name_path_1) <= Enum.join(name_path_2)
+    end)
+    |> then(&{:reply, &1, state})
   end
 end
--- test/alias_formatter/context_alias_collector_test.exs
+++ test/alias_formatter/context_alias_collector_test.exs
@@ -9,22 +9,43 @@ defmodule AliasFormatter.ContextAliasCollectorTest do
     [pid: pid]
   end
 
-  test "should return existing short alias version", %{pid: pid} do
-    {name_path, as_alias} = test_alias_data = {[:Test, :Module], :AsThis}
+  describe "get_short_name/2" do
+    test "should return existing short alias version", %{pid: pid} do
+      {name_path, as_alias} = test_alias_data = {[:Test, :Module], :AsThis}
 
-    ContextAliasCollector.add_alias(pid, test_alias_data)
+      ContextAliasCollector.add_alias(pid, test_alias_data)
 
-    assert ^as_alias = ContextAliasCollector.get_short_name(pid, name_path)
+      assert ^as_alias = ContextAliasCollector.get_short_name(pid, name_path)
+    end
+
+    test "get_short_name/2 should add new alias", %{pid: pid} do
+      {name_path, as_alias} = test_alias_data = {[:Test, :Module], :AsThis}
+      new_last_name = :Aaa
+      new_name_path = name_path ++ [new_last_name]
+
+      ContextAliasCollector.add_alias(pid, test_alias_data)
+
+      assert ^as_alias = ContextAliasCollector.get_short_name(pid, name_path)
+      assert ^new_last_name = ContextAliasCollector.get_short_name(pid, new_name_path)
+    end
   end
 
-  test "should add new alias", %{pid: pid} do
-    {name_path, as_alias} = test_alias_data = {[:Test, :Module], :AsThis}
-    new_last_name = :Aaa
-    new_name_path = name_path ++ [new_last_name]
+  describe "get_result_aliases/1" do
+    test "should return result aliases as sorted list", %{pid: pid} do
+      [
+        {[:Aaa, :Bbb, :Ccc], :CccAliasAs},
+        {[:Aaa, :Bbb], :Bbb},
+        {[:Aaa, :Yyy], :YyyAliasAs}
+      ]
+      |> Enum.each(fn test_alias_data ->
+        ContextAliasCollector.add_alias(pid, test_alias_data)
+      end)
 
-    ContextAliasCollector.add_alias(pid, test_alias_data)
-
-    assert ^as_alias = ContextAliasCollector.get_short_name(pid, name_path)
-    assert ^new_last_name = ContextAliasCollector.get_short_name(pid, new_name_path)
+      assert [
+               {[:Aaa, :Bbb], :Bbb},
+               {[:Aaa, :Bbb, :Ccc], :CccAliasAs},
+               {[:Aaa, :Yyy], :YyyAliasAs}
+             ] = ContextAliasCollector.get_result_aliases(pid)
+    end
   end
 end
