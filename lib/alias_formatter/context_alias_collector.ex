ffddbdaebecfdc39895f808296d37697820b9a62
Misha Pachin <misha.pachin@gmail.com>
2023-04-10 11:08:07 +0200

    Update ContextAliasCollector tests and implementation

--- lib/alias_formatter/context_alias_collector.ex
+++ lib/alias_formatter/context_alias_collector.ex
@@ -1,39 +1,43 @@
 defmodule AliasFormatter.ContextAliasCollector do
   use GenServer
 
+  alias AliasFormatter.ContextAliasCollector.NamePathsMap
+
+  def add_alias(pid, {name_path, alias_as} = alias_data)
+      when is_list(name_path) and is_atom(alias_as) do
+    GenServer.cast(pid, {:add_alias, alias_data})
+  end
+
+  def get_short_name(pid, name_path) when is_list(name_path) do
+    GenServer.call(pid, {:get_short_name, name_path})
+  end
+
   def start_link(_) do
     {:ok, pid} = GenServer.start_link(__MODULE__, [])
     pid
   end
 
-  def add_alias_data(pid, {_, _} = alias_data) do
-    GenServer.cast(pid, {:add_alias, alias_data, alias_data})
-  end
-
-  def add_alias_data(pid, {_, _} = was, {_, _} = now) do
-    GenServer.cast(pid, {:add_alias, was, now})
-  end
-
-  def get_short_name(pid, module_atoms_name) when is_list(module_atoms_name) do
-    GenServer.call(pid, {:get_short_name})
-  end
-
   @impl true
   def init(_) do
-    {:ok, %{}}
+    paths_to_leaf = %{}
+    leaf_to_path = %{}
+    leaf_to_increment = %{}
+    {:ok, {paths_to_leaf, leaf_to_path, leaf_to_increment}}
   end
 
   @impl true
-  def handle_cast({:add_alias, {alias_atoms_name, alias_as}}, state) do
+  def handle_cast({:add_alias, alias_data}, state) do
     state
-    |> Map.put(alias_atoms_name, alias_as)
+    |> NamePathsMap.add_alias(alias_data)
     |> then(&{:noreply, &1})
   end
 
   @impl true
-  def handle_call({:get_short_name, alias_atoms_name}, _from, state) do
+  def handle_call({:get_short_name, name_path}, _from, state) do
     state
-    |> Map.get(alias_atoms_name)
-    |> then(&{:reply, &1, state})
+    |> NamePathsMap.get_alias_short_form(name_path)
+    |> then(fn {short_name, updated_state} ->
+      {:reply, short_name, updated_state}
+    end)
   end
 end
--- test/alias_formatter/context_alias_collector_test.exs
+++ test/alias_formatter/context_alias_collector_test.exs
@@ -9,12 +9,22 @@ defmodule AliasFormatter.ContextAliasCollectorTest do
     [pid: pid]
   end
 
-  describe "get_short_module_name" do
-    test "should return short alias version", %{pid: pid} do
-      test_alias_data = {[:Test, :Module], :AsThis}
-      ContextAliasCollector.add_alias_data(pid, test_alias_data)
+  test "should return existing short alias version", %{pid: pid} do
+    {name_path, as_alias} = test_alias_data = {[:Test, :Module], :AsThis}
 
-      assert [:AsThis] = ContextAliasCollector.get_short_name(pid, [:Test, :Module])
-    end
+    ContextAliasCollector.add_alias(pid, test_alias_data)
+
+    assert ^as_alias = ContextAliasCollector.get_short_name(pid, name_path)
+  end
+
+  test "should add new alias", %{pid: pid} do
+    {name_path, as_alias} = test_alias_data = {[:Test, :Module], :AsThis}
+    new_last_name = :Aaa
+    new_name_path = name_path ++ [new_last_name]
+
+    ContextAliasCollector.add_alias(pid, test_alias_data)
+
+    assert ^as_alias = ContextAliasCollector.get_short_name(pid, name_path)
+    assert ^new_last_name = ContextAliasCollector.get_short_name(pid, new_name_path)
   end
 end
