77131867a465c30f3cfe6438062aba818d79f4ab
Misha Pachin <misha.pachin@gmail.com>
2023-04-16 08:08:34 +0200

    Update AliasSubstitute * Make substitute/2 private * Add public function retrieve_aliases_from_ast/2 * Update tests

--- lib/alias_formatter/defmodule_substitute/alias_substitute.ex
+++ lib/alias_formatter/defmodule_substitute/alias_substitute.ex
@@ -1,53 +1,63 @@
 defmodule AliasFormatter.DefmoduleSubstitute.AliasSubstitute do
+  alias AliasFormatter.AST
   alias AliasFormatter.ContextAliasCollector
 
-  def substitute(
-        {:alias, _, [{:__aliases__, _, name_path}]},
-        alias_collector_pid
-      ) do
+  def retrieve_aliases_from_ast(ast_list, alias_collector_pid) do
+    {alias_asts_list, rest_asts_list} = AST.split_aliases(ast_list)
+
+    alias_asts_list
+    |> Enum.each(&substitute(&1, alias_collector_pid))
+
+    rest_asts_list
+  end
+
+  defp substitute(
+         {:alias, _, [{:__aliases__, _, name_path}]},
+         alias_collector_pid
+       ) do
     alias_as = List.last(name_path)
 
     alias_collector_pid
     |> ContextAliasCollector.add_alias({name_path, alias_as})
   end
 
-  def substitute(
-        {:alias, _,
-         [
-           {:__aliases__, _, name_path},
-           [
-             {
-               {:__block__, _, [:as]},
-               {:__aliases__, _, alias_as_list}
-             }
-           ]
-         ]},
-        alias_collector_pid
-      ) do
+  defp substitute(
+         {:alias, _,
+          [
+            {:__aliases__, _, name_path},
+            [
+              {
+                {:__block__, _, [:as]},
+                {:__aliases__, _, alias_as_list}
+              }
+            ]
+          ]},
+         alias_collector_pid
+       ) do
     alias_as = List.last(alias_as_list)
 
     alias_collector_pid
     |> ContextAliasCollector.add_alias({name_path, alias_as})
   end
 
-  def substitute(
-        {
-          :alias,
-          _,
-          [
-            {
-              {:., _,
-               [
-                 {:__aliases__, _, partial_name_path},
-                 :{}
-               ]},
-              _,
-              alias_postfixes_list
-            }
-          ]
-        },
-        alias_collector_pid
-      ) do
+  defp substitute(
+         {
+           :alias,
+           _,
+           [
+             {
+               {:., _,
+                [
+                  {:__aliases__, _, partial_name_path},
+                  :{}
+                ]},
+               _,
+               alias_postfixes_list
+             }
+           ]
+         },
+         alias_collector_pid
+       ) do
     alias_postfixes_list
     |> Enum.each(fn {:__aliases__, _, alias_postfix_atoms} ->
       full_alias_name_path = partial_name_path ++ alias_postfix_atoms
@@ -58,5 +68,5 @@ defmodule AliasFormatter.DefmoduleSubstitute.AliasSubstitute do
     end)
   end
 
-  def substitute(ast_fragment, _alias_collector_pid), do: ast_fragment
+  defp substitute(ast_fragment, _alias_collector_pid), do: ast_fragment
 end
--- test/alias_formatter/defmodule_substitute/alias_substitute_test.exs
+++ test/alias_formatter/defmodule_substitute/alias_substitute_test.exs
@@ -9,23 +9,50 @@ defmodule AliasFormatter.DefmoduleSubstitute.AliasSubstituteTest do
     [pid: pid]
   end
 
-  describe "substitute/2" do
-    test "should populate alias collector with simple alias", %{pid: pid} do
-      test_last_module = :Three
-      test_name_path = [:One, :Two, test_last_module]
+  describe "retrieve_aliases_from_ast/2" do
+    test "should remove aliases from ast list and populate alias collector", %{pid: pid} do
+      expected_alias_list = [
+        {{:., [line: 4], [{:__aliases__, [line: 4], [:Three]}, :fun]}, [line: 4], []}
+      ]
 
-      {:alias, [line: 2], [{:__aliases__, [line: 2], test_name_path}]}
-      |> AliasSubstitute.substitute(pid)
+      {alias_ast_list, retrieved_aliases} =
+        [
+          get_test_simple_form_alias(),
+          get_test_alias_as_form(),
+          get_test_long_alias_as_form(),
+          get_test_short_form_alias()
+        ]
+        |> Enum.unzip()
 
-      assert [{^test_name_path, ^test_last_module}] =
-               ContextAliasCollector.get_result_aliases(pid)
+      test_ast_list = alias_ast_list ++ expected_alias_list
+
+      expected_retrieved_aliases = Enum.concat(retrieved_aliases)
+
+      assert ^expected_alias_list =
+               test_ast_list
+               |> AliasSubstitute.retrieve_aliases_from_ast(pid)
+
+      assert ^expected_retrieved_aliases = ContextAliasCollector.get_result_aliases(pid)
     end
+  end
 
-    test "should populate alias collector with alias as", %{pid: pid} do
-      expected_alias_as = :As
-      test_name_path = [:One, :Two, :Three]
-      alias_as_list = [expected_alias_as]
+  defp get_test_simple_form_alias do
+    test_last_module = :Two
+    test_name_path = [:One, test_last_module]
 
+    simple_form_alias = {:alias, [line: 2], [{:__aliases__, [line: 2], test_name_path}]}
+
+    expected_retrieved_simple_form_aliases = [{test_name_path, test_last_module}]
+
+    {simple_form_alias, expected_retrieved_simple_form_aliases}
+  end
+
+  defp get_test_alias_as_form do
+    expected_alias_as = :As
+    test_name_path = [:One, :Two, :Three]
+    alias_as_list = [expected_alias_as]
+
+    alias_as_form =
       {:alias, [line: 2],
        [
          {:__aliases__, [line: 2], test_name_path},
@@ -34,17 +61,18 @@ defmodule AliasFormatter.DefmoduleSubstitute.AliasSubstituteTest do
             {:__aliases__, [line: 2], alias_as_list}}
          ]
        ]}
-      |> AliasSubstitute.substitute(pid)
 
-      assert [{^test_name_path, ^expected_alias_as}] =
-               ContextAliasCollector.get_result_aliases(pid)
-    end
+    expected_retrieved_alias_as_form = [{test_name_path, expected_alias_as}]
 
-    test "should populate alias collector with long alias as", %{pid: pid} do
-      expected_alias_as = :As
-      test_name_path = [:One, :Two, :Three]
-      alias_as_list = [:Ignored, :Alias, :As, :Parts, expected_alias_as]
+    {alias_as_form, expected_retrieved_alias_as_form}
+  end
 
+  defp get_test_long_alias_as_form do
+    expected_alias_as = :LongAliasAs
+    test_name_path = [:One, :Two, :Three, :Four]
+    alias_as_list = [:Ignored, :Alias, :As, :Parts, expected_alias_as]
+
+    long_alias_as_form =
       {:alias, [line: 2],
        [
          {:__aliases__, [line: 2], test_name_path},
@@ -53,36 +81,37 @@ defmodule AliasFormatter.DefmoduleSubstitute.AliasSubstituteTest do
             {:__aliases__, [line: 2], alias_as_list}}
          ]
        ]}
-      |> AliasSubstitute.substitute(pid)
 
-      assert [{^test_name_path, ^expected_alias_as}] =
-               ContextAliasCollector.get_result_aliases(pid)
-    end
+    expected_retrieved_long_alias_as_form = [{test_name_path, expected_alias_as}]
 
-    test "should populate alias collector with unwrapped short form aliases", %{pid: pid} do
-      test_name_path = [:One, :Two, :Three]
-      name_path_postfix_1 = [:Postfix, :First]
-      name_path_postfix_2 = [:Postfix, :Second]
+    {long_alias_as_form, expected_retrieved_long_alias_as_form}
+  end
 
-      expected_name_path_1 = test_name_path ++ name_path_postfix_1
-      expected_name_path_2 = test_name_path ++ name_path_postfix_2
-      expected_alias_as_1 = List.last(name_path_postfix_1)
-      expected_alias_as_2 = List.last(name_path_postfix_2)
+  defp get_test_short_form_alias do
+    test_name_path = [:One, :Two, :Three, :Four, :Five]
+    name_path_postfix_1 = [:Postfix, :First]
+    name_path_postfix_2 = [:Postfix, :Second]
 
-      {:alias, [line: 2],
+    expected_name_path_1 = test_name_path ++ name_path_postfix_1
+    expected_name_path_2 = test_name_path ++ name_path_postfix_2
+    expected_alias_as_1 = List.last(name_path_postfix_1)
+    expected_alias_as_2 = List.last(name_path_postfix_2)
+
+    short_form_alias =
+      {:alias, [line: 3],
        [
-         {{:., [line: 2], [{:__aliases__, [line: 2], test_name_path}, :{}]}, [line: 2],
+         {{:., [line: 3], [{:__aliases__, [line: 3], test_name_path}, :{}]}, [line: 3],
           [
-            {:__aliases__, [line: 2], name_path_postfix_1},
-            {:__aliases__, [line: 2], name_path_postfix_2}
+            {:__aliases__, [line: 3], name_path_postfix_1},
+            {:__aliases__, [line: 3], name_path_postfix_2}
           ]}
        ]}
-      |> AliasSubstitute.substitute(pid)
 
-      assert [
-               {^expected_name_path_1, ^expected_alias_as_1},
-               {^expected_name_path_2, ^expected_alias_as_2}
-             ] = ContextAliasCollector.get_result_aliases(pid)
-    end
+    expected_retrieved_short_form_aliases = [
+      {expected_name_path_1, expected_alias_as_1},
+      {expected_name_path_2, expected_alias_as_2}
+    ]
+
+    {short_form_alias, expected_retrieved_short_form_aliases}
   end
 end
