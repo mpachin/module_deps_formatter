f310fa81b2d0e32c98f95f534c0608ae1e98c0c4
Misha Pachin <misha.pachin@gmail.com>
2023-04-16 10:23:18 +0200

    Update DefSubstitute * Reuse AliasSubstitute.retrieve_aliases_from_ast/2 for retrieving alias asts

--- lib/alias_formatter/defmodule_substitute/def_substitute.ex
+++ lib/alias_formatter/defmodule_substitute/def_substitute.ex
@@ -1,6 +1,5 @@
 defmodule AliasFormatter.DefmoduleSubstitute.DefSubstitute do
-  alias AliasFormatter.AST
-  alias AliasFormatter.ContextAliasCollector
+  alias AliasFormatter.DefmoduleSubstitute.AliasSubstitute
 
   def substitute(
         {
@@ -22,7 +21,8 @@ defmodule AliasFormatter.DefmoduleSubstitute.DefSubstitute do
         },
         alias_collector_pid
       ) do
-    updated_ast_block = retrieve_aliases(second_block_ast, alias_collector_pid)
+    updated_ast_block =
+      AliasSubstitute.retrieve_aliases_from_ast(second_block_ast, alias_collector_pid)
 
     do_block_options
     |> Keyword.get(:format)
@@ -61,34 +61,4 @@ defmodule AliasFormatter.DefmoduleSubstitute.DefSubstitute do
   def unwrap_ast([]), do: nil
 
   def unwrap_ast(ast_block), do: ast_block
-
-  defp retrieve_aliases(ast_block, alias_collector_pid) do
-    {alias_asts_list, rest_asts_list} = AST.split_aliases(ast_block)
-
-    alias_asts_list
-    |> Enum.each(fn alias_ast ->
-      alias_ast
-      |> alias_ast_to_pair()
-      |> then(&ContextAliasCollector.add_alias(alias_collector_pid, &1))
-    end)
-
-    rest_asts_list
-  end
-
-  defp alias_ast_to_pair({:alias, _, [{:__aliases__, _, name_path}]}) do
-    alias_as = List.last(name_path)
-    {name_path, alias_as}
-  end
-
-  defp alias_ast_to_pair(
-         {:alias, _,
-          [
-            {:__aliases__, _, name_path},
-            [
-              {{:__block__, _, [:as]}, {:__aliases__, _, alias_as}}
-            ]
-          ]}
-       ) do
-    {name_path, alias_as}
-  end
 end
