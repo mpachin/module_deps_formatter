943cb162eb5e8c5073f2cf4c94cba767129c0b82
Misha Pachin <misha.pachin@gmail.com>
2023-04-17 06:48:16 +0200

    Update AST.File.substitute/1 * Make it retrieve result alias asts from context alias collector via Alias.get_result_context_aliases/1 * Retrieved (and sorted) alias asts prepended to the result ast list * Update tests

--- lib/alias_formatter/ast/file.ex
+++ lib/alias_formatter/ast/file.ex
@@ -4,20 +4,34 @@ defmodule AliasFormatter.AST.File do
   alias AliasFormatter.ContextAliasCollector
 
   def substitute({:alias, _, _} = alias_ast) do
-    alias_collector_pid = ContextAliasCollector.start_link([])
+    alias_collector_pid = get_alias_collector_pid()
 
     [alias_ast]
     |> Alias.retrieve_aliases_from_ast(alias_collector_pid)
 
-    {:__block__, [], []}
+    result_context_aliases = Alias.get_result_context_aliases(alias_collector_pid)
+
+    {:__block__, [], result_context_aliases}
   end
 
   def substitute({:__block__, [], ast_content_list}) do
-    alias_collector_pid = ContextAliasCollector.start_link([])
+    alias_collector_pid = get_alias_collector_pid()
 
-    ast_content_list
-    |> Alias.retrieve_aliases_from_ast(alias_collector_pid)
-    |> Enum.map(&Defmodule.substitute(&1, alias_collector_pid))
-    |> then(&{:__block__, [], &1})
+    processed_ast =
+      ast_content_list
+      |> Alias.retrieve_aliases_from_ast(alias_collector_pid)
+      |> Enum.map(fn content_ast ->
+        # TODO: move its creation into AST.Defmodule
+        content_level_pid = get_alias_collector_pid()
+        Defmodule.substitute(content_ast, content_level_pid)
+      end)
+
+    result_context_aliases = Alias.get_result_context_aliases(alias_collector_pid)
+
+    {:__block__, [], result_context_aliases ++ processed_ast}
+  end
+
+  defp get_alias_collector_pid do
+    ContextAliasCollector.start_link([])
   end
 end
--- test/alias_formatter/ast/file_test.exs
+++ test/alias_formatter/ast/file_test.exs
@@ -2,18 +2,17 @@ defmodule AliasFormatter.AST.FileTest do
   use ExUnit.Case
 
   alias AliasFormatter.AST.File
-  alias AliasFormatter.ContextAliasCollector
 
   describe "substitute/2" do
-    test "should retrieve alias when it is the only content" do
+    test "should process alias when it is the only content" do
       last_module_name = :Three
       name_path = [:One, :Two, last_module_name]
       alias_ast = get_alias_ast(name_path)
 
-      assert {:__block__, [], []} = File.substitute(alias_ast)
+      assert {:__block__, [], [^alias_ast]} = File.substitute(alias_ast)
     end
 
-    test "should retrieve alias when it have other content" do
+    test "should process alias when file ast have other content" do
       last_module_name = :Three
       name_path = [:One, :Two, last_module_name]
       file_level_alias_ast = get_alias_ast(name_path)
@@ -23,7 +22,13 @@ defmodule AliasFormatter.AST.FileTest do
       defmodule_ast = get_defmodule_ast([module_level_alias_ast])
 
       input_ast = get_file_ast([file_level_alias_ast, defmodule_ast])
-      expected_ast = [get_defmodule_ast()] |> get_file_ast()
+
+      expected_ast =
+        [
+          file_level_alias_ast,
+          get_defmodule_ast()
+        ]
+        |> get_file_ast()
 
       assert ^expected_ast = File.substitute(input_ast)
     end
